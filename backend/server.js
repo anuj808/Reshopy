02a30e7a844d5ac9ab3e671b5649c1deb52ac659
anuj808 <anujrajbhar809@gmail.com>
2026-04-09 19:01:56 +0530

    backend connection

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,15 @@ const app = express();
 
 // Middleware
 app.use(express.json());
-app.use(cors());
+app.use(
+  cors({
+    origin: [
+      "http://localhost:3000",         
+      "https://reshopy.vercel.app",   
+    ],
+    credentials: true,
+  })
+);
 
 // DB
 connectDB();
